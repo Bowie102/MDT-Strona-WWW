d6ebfefb84450230b2d631ede267fd914e6c7c73
Bowie102 <bowie102938@gmail.com>
2026-06-22 23:42:27 +0200

    Add files via upload

--- src/pages/FTD.jsx
+++ src/pages/FTD.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { X } from 'lucide-react';
 import { API_BASE_URL } from '../config';
 
 const TRAININGS = ['SEU', 'SV', 'NT', 'PWC', 'WU', 'K9', 'ASU', 'MARY', 'FAC'];
